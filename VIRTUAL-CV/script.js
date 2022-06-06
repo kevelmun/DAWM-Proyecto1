70ad279f0a999b881fb436bf130c7455344fb285
kevelmun <kevelmun@espol.edu.ec>
2022-06-05 23:20:09 -0500

    ScrollBar

---
+++ VIRTUAL-CV/script.js
@@ -0,0 +1,9 @@
+function scrolll() {
+    var left = document.querySelector(".scrollSection");
+    left.scrollBy(-350, 0)
+}
+
+function scrollr() {
+var right = document.querySelector(".scrollSection");
+right.scrollBy(350, 0)
+}
