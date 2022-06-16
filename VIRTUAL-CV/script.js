89e02679a44e1b4ab9ee7ad9cbee361e7903ad0b
kevelmun <kevelmun@espol.edu.ec>
2022-06-16 18:28:17 -0500

    Responsive

--- VIRTUAL-CV/script.js
+++ VIRTUAL-CV/script.js
@@ -7,3 +7,13 @@ function scrollr() {
 var right = document.querySelector(".scrollSection");
 right.scrollBy(350, 0)
 }
+
+let menu = document.querySelector("#menu");
+let menuBarra = document.querySelector("#menuButton")
+
+menuBarra.addEventListener("click", function(){
+    menu.classList.toggle("menuButton");
+})
+
+let audio = document.getElementById("backgroudMusic")
+audio.volume = 0.2;
